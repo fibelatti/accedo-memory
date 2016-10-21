4fbb205d707ca86294548a2ed900b9f151a3e164
Filipe Belatti <fibelatti@gmail.com>
2016-10-21 18:35:10 +0200

    Creating memory game activity. Icons are should be replaced.

---
+++ app/src/main/java/com/fibelatti/accedomemory/views/activities/MemoryGameActivity.java
@@ -0,0 +1,80 @@
+package com.fibelatti.accedomemory.views.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.fibelatti.accedomemory.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MemoryGameActivity extends AppCompatActivity {
+    private Context context;
+
+    @BindView(R.id.coordinator_layout)
+    CoordinatorLayout layout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = getApplicationContext();
+
+        setUpLayout();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_memory_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new_game:
+                return true;
+            case R.id.action_high_scores:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setUpLayout() {
+        setContentView(R.layout.activity_memory_game);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+//        setUpRecyclerView();
+    }
+
+//    private void setUpRecyclerView() {
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.setAdapter(adapter);
+//
+//        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this, new OnItemClickListener() {
+//            @Override
+//            public void onItemTouch(View view, int position) {
+//
+//            }
+//        }));
+//    }
+}
