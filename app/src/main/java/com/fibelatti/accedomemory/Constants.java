013401b0fce7a7862668a20b525565689433317e
Filipe Belatti <fibelatti@gmail.com>
2016-10-25 22:07:24 +0200

    Creating constants file

---
+++ app/src/main/java/com/fibelatti/accedomemory/Constants.java
@@ -0,0 +1,7 @@
+package com.fibelatti.accedomemory;
+
+public abstract class Constants {
+    public static final int SCORE_SUCCESS = 5;
+    public static final int SCORE_FAILURE = -1;
+    public static final int ROUND_DELAY = 1000;
+}
