4dc950d6607ba3b8baeff04ff410dff5ee5e4c92
Filipe Belatti <fibelatti@gmail.com>
2016-10-25 22:08:44 +0200

    Creating new model for event bus posting

---
+++ app/src/main/java/com/fibelatti/accedomemory/models/MatchResult.java
@@ -0,0 +1,13 @@
+package com.fibelatti.accedomemory.models;
+
+public class MatchResult {
+    boolean matched;
+
+    public MatchResult(boolean matched) {
+        this.matched = matched;
+    }
+
+    public boolean isMatched() {
+        return matched;
+    }
+}
